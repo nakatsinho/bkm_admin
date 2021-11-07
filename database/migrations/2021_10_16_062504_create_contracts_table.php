5c6bb0aaab1e7fb317fb5a38c78026f3f8b9d5c0
nakatsinho <nakatsinho@gmail.com>
2021-11-07 09:13:42 +0200

    New: Migration for contracts

---
+++ database/migrations/2021_10_16_062504_create_contracts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContractsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nid')->nullable();
+            $table->string('from')->nullable();
+            $table->string('to')->nullable();
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+}
