48683ba02ab4e866559fd9f5c9819c054eb62a68
nakatsinho <nakatsinho@gmail.com>
2021-11-07 09:18:13 +0200

    New: Footer for app

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                    <div class="col-sm-6">
+                        Copyright &copy; {{date('Y')}} <a href="https://legocode.tech">Legocode, Lda</a>
+                    </div>
+                    <div class="col-sm-6 text-right">
+                        Powered by <a href="https://wa.me/+258825248888">Kelton M. Cumbe</a>.
+                    </div>
+                </div>
+            </div>
+        </footer>
