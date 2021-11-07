8b1cc0c746a7b6721496013f9f611569ebc5abe1
nakatsinho <nakatsinho@gmail.com>
2021-11-07 09:14:46 +0200

    Update: Updating user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
+            $table->string('surname');
+            $table->string('local');
+            $table->string('contact1');
+            $table->string('contact2');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('adrress')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
