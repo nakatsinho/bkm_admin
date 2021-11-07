1777a40bf2de119886b80fa0ddfe6c62a19dcfa2
nakatsinho <nakatsinho@gmail.com>
2021-11-07 09:08:27 +0200

    New: Home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contract;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $contract = Contract::all();
+        $users = User::all();
+
+        return view('home', compact('contract', 'users'));
+    }
+}
