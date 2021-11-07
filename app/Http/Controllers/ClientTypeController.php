c6e8b674902f94b049dbc6d1825e2a2dc810e0b7
nakatsinho <nakatsinho@gmail.com>
2021-11-07 09:07:44 +0200

    New: Client Type controller

---
+++ app/Http/Controllers/ClientTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client_Type;
+use Illuminate\Http\Request;
+
+class ClientTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Client_Type  $client_Type
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client_Type $client_Type)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Client_Type  $client_Type
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Client_Type $client_Type)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Client_Type  $client_Type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Client_Type $client_Type)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Client_Type  $client_Type
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client_Type $client_Type)
+    {
+        //
+    }
+}
