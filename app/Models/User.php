2482902e4f4dc9b2699d1652f223abaf52010c3b
nakatsinho <nakatsinho@gmail.com>
2021-11-07 09:11:01 +0200

    Update: Adding new fields to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,12 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'surname',
+        'local',
+        'contact1',
+        'contact2',
+        'email',
+        'address',
         'password',
     ];
 
